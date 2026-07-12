427e94e2f49300a4c3cbed6ba673d71cb841318b
heineken <lenguyenquanduy@gmail.com>
2026-07-12 15:41:54 +0700

    Refactor component creation to use CreateComponentRequest struct, enhancing type safety and clarity

--- src/web/routes/components.rs
+++ src/web/routes/components.rs
@@ -1,12 +1,30 @@
 use axum::{Json, Router, extract::{Path, Query, State}, http::StatusCode, response::IntoResponse, routing::{get, post}};
-use serde::Serialize;
-use crate::{core::components::{models::{payload::ComponentPayload, wrapper::Component}, repositories::{ComponentFilterQuery, ComponentsRepository}}, web::routes::context::Context};
+use serde::{Deserialize, Serialize};
+use crate::{core::components::{models::{payload::ComponentPayload, values::version::Version, wrapper::Component}, repositories::{ComponentFilterQuery, ComponentsRepository}}, web::routes::context::Context};
 
 pub fn build() -> Router<Context> {
     Router::new()
         .route("/components", post(add_new_component))
         .route("/components", get(get_all_latest_components))
         .route("/components/{id}", get(get_latest_component))
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields)]
+struct CreateComponentRequest<T> {
+    pub title: String,
+    pub payload: T,
+}
+
+impl<T> From<CreateComponentRequest<T>> for Component<T> {
+    fn from(req: CreateComponentRequest<T>) -> Self {
+        Component { 
+            id: None, 
+            version: Version::default(), 
+            title: req.title, 
+            payload: req.payload 
+        }
+    }
 }
 
 #[derive(Serialize)]
@@ -16,9 +34,9 @@ struct CreatedResponse {
 
 async fn add_new_component(
     State(ctx): State<Context>, 
-    Json(component): Json<Component<ComponentPayload>>
+    Json(request): Json<CreateComponentRequest<ComponentPayload>>
 ) -> impl IntoResponse {
-    match ctx.create_new(&component).await {
+    match ctx.create_new(&request.into()).await {
         Ok(generated_id) => {
             (
                 StatusCode::CREATED, 
