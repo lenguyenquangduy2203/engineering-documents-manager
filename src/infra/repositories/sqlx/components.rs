e6ee23bead562634cfe6562ac47fb172bf0febb8
heineken <lenguyenquanduy@gmail.com>
2026-07-18 18:09:20 +0700

    Implement ComponentRef conversion and add ComponentTypeResolver for SqliteComponentRepository; enhance component retrieval by version IDs

--- src/infra/repositories/sqlx/components.rs
+++ src/infra/repositories/sqlx/components.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
 
-use anyhow::anyhow;
+use anyhow::{Ok, anyhow};
 use async_trait::async_trait;
 use serde_json::Value;
 use sqlx::{Executor, Pool, QueryBuilder, Row, Sqlite, Transaction, sqlite::SqliteRow};
 
-use crate::core::components::{models::{payload::ComponentPayload, values::version::Version, wrapper::Component}, queries::component::ComponentQuery, repositories::{ComponentFilterQuery, ComponentsRepository}};
+use crate::core::components::{models::{payload::ComponentPayload, values::version::Version, wrapper::Component}, queries::component::ComponentQuery, repositories::{ComponentFilterQuery, ComponentRef, ComponentTypeResolver, ComponentsRepository}};
 
 impl TryFrom<SqliteRow> for Component<ComponentPayload> {
     type Error = anyhow::Error;
@@ -22,8 +22,6 @@ impl TryFrom<SqliteRow> for Component<ComponentPayload> {
         })
     }
 }
-
-
 
 pub struct SqliteComponentRepository {
     dbc: Arc<Pool<Sqlite>>,
@@ -210,5 +208,49 @@ impl ComponentsRepository for SqliteComponentRepository {
         tx.commit().await?;
 
         Ok(())
+    }
+}
+
+impl TryFrom<SqliteRow> for ComponentRef {
+    type Error = anyhow::Error;
+
+    fn try_from(row: SqliteRow) -> Result<Self, Self::Error> {
+        Ok(Self { 
+            id: row.get("component_id"), 
+            version_id: row.get("version_id"), 
+            component_type: row.get("component_type") 
+        })
+    }
+}
+
+#[async_trait]
+impl ComponentTypeResolver for SqliteComponentRepository {
+    async fn find_all_components_with_type_by_version_ids(
+        &self,
+        version_ids: &[u32]
+    ) -> anyhow::Result<Vec<ComponentRef>> {
+        if version_ids.is_empty() {
+            return Ok(Vec::new());
+        }
+
+        let json_ids = serde_json::to_string(version_ids)?;
+        let rows = sqlx::query(
+            r#"
+            SELECT 
+                c.id AS component_id, 
+                v.id AS version_id,
+                c.type AS component_type
+            FROM components c
+            LEFT JOIN component_versions v ON c.id = v.component_id
+            WHERE v.id IN (SELECT value FROM json_each($1))
+            "#
+        )
+        .bind(json_ids)
+        .fetch_all(&*self.dbc)
+        .await?;
+
+        rows.into_iter()
+            .map(ComponentRef::try_from)
+            .collect()
     }
 }
